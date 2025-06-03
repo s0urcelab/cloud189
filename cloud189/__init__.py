b8a6ad8bf721fb07d99f7a92f04885ee85d6d611
s0urce <me@src.moe>
2025-06-03 10:12:03 +0800

    feat: auto version code for pypi

--- cloud189/__init__.py
+++ cloud189/__init__.py
@@ -4,5 +4,5 @@ Cloud189 SDK - A Python SDK for 189 Cloud (天翼网盘)
 
 from .client import CloudClient as Cloud189
 
-__version__ = '0.1.4'
+__version__ = '<PYPI_VERSION>'
 __all__ = ['Cloud189'] 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cloud189",
-    version="0.1.4",
+    version="<PYPI_VERSION>",
     author="s0urce",
     author_email="me@src.moe",
     description="A Python SDK for interacting with Cloud189",
